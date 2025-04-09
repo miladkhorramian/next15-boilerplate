18fb50625a5f3e3701cc8334ad4b948c30f23831
Milad Khorramian <mahdiar.khorramian@yahoo.com>
2025-04-09 11:58:04 +0330

    UPDAEE: Fixed initial main branch directory setup.

---
+++ src/app/sample/page.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+function SamplePage() {
+  return (
+    <div className="p-5 *:pb-3">
+      <p className="">Application Page</p>
+    </div>
+  );
+}
+
+export default SamplePage;
